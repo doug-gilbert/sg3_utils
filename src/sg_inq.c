doug-gilbert/sg3_utils

commit 2fe14529c6103842bc9e327c5d7063638a1b31e9

sg_inq: --export output conformance for SCSI name string and ATA fields (github: PR #83); Apply udev-conforming character encoding to VPD 0x83 designator type 8 (SCSI name string) and the T10 vendor ID ATA subfield, matching the encoding already used by designator types 0 and 1 since commit 4d770f1
git-svn-id: https://svn.bingwo.ca/repos/sg3_utils/trunk@1125 6180dd3e-e324-4e3e-922d-17de1ae2f315

src/sg_inq.c
@@ -1590,8 +1590,17 @@ export_dev_ids(uint8_t * buff, int len, int verbose)
                 }
                 printf("\n");
                 if (!memcmp(ip, "ATA_", 4)) {
-                    printf("SCSI_IDENT_%s_ATA=%.*s\n", assoc_str,
-                           k - 4, ip + 4);
+                    printf("SCSI_IDENT_%s_ATA=", assoc_str);
+                    for (m = 4; m < k; ++m) {
+                        if ((ip[m] >= '0' && ip[m] <= '9') ||
+                            (ip[m] >= 'A' && ip[m] <= 'Z') ||
+                            (ip[m] >= 'a' && ip[m] <= 'z') ||
+                            strchr("#+-.:=@_", ip[m]) != NULL)
+                            printf("%c", ip[m]);
+                        else
+                            printf("\\x%02x", ip[m]);
+                    }
+                    printf("\n");
                 }
             } else {
                 for (m = 0; m < i_len; ++m)
@@ -1714,8 +1723,17 @@ export_dev_ids(uint8_t * buff, int len, int verbose)
                 break;
             }
 
-            printf("SCSI_IDENT_%s_NAME=%.*s\n", assoc_str, i_len,
-                   (const char *)ip);
+            printf("SCSI_IDENT_%s_NAME=", assoc_str);
+            for (m = 0; m < i_len; ++m) {
+                if ((ip[m] >= '0' && ip[m] <= '9') ||
+                    (ip[m] >= 'A' && ip[m] <= 'Z') ||
+                    (ip[m] >= 'a' && ip[m] <= 'z') ||
+                    strchr("#+-.:=@_", ip[m]) != NULL)
+                    printf("%c", ip[m]);
+                else
+                    printf("\\x%02x", ip[m]);
+            }
+            printf("\n");
             break;
         case 9: /*  Protocol specific port identifier */
             if (TPROTO_UAS == p_id) {

inhex/tst_script.sh
@@ -89,5 +89,19 @@ sg_vpd -I vpd_zbdc.hex
 
 sg_z_act_query --inhex=z_act_query.hex
 
+echo ""
+echo ">>>>>>>>>>>>>>>> sg_inq --export udev encoding conformance test"
+# VPD 0x83 designator type 8 (SCSI name string) udev encoding conformance
+# test. The --export output must be in conformance with other udev character
+# encoding rules.
+export_output=$(sg_inq --export --inhex=vpd_di_name_inject.hex 2>/dev/null)
+export_lines=$(echo "$export_output" | wc -l)
+if [ "$export_lines" -eq 1 ] && echo "$export_output" | grep -q '\\x0a'; then
+    echo "PASS: SCSI name string is udev conformed character encoding ($export_lines line)"
+else
+    echo "FAIL: expected udev conformance, instead got $export_lines line(s):"
+    echo "$export_output"
+fi
+
 # D. Gilbert, last updated 20230420
 

inhex/vpd_di_name_inject.hex
@@ -1,0 +1,18 @@
+#
+# VPD page 0x83 with a SCSI name string (designator type 8) testing
+# udev udev-conforming character encoding.
+#
+# Used to test that sg_inq --export conforms as expected.
+#
+# Identifier: iqn.2026-05.sg3\nX_SG3_PROBE=1
+#
+# An example invocation:
+#    sg_inq --export --inhex=vpd_di_name_inject.hex --raw
+
+00 83 00 24
+
+# SCSI name string designator (type 8, code set 3 UTF-8, association 0 LUN)
+# descriptor header: code_set=3, assoc=0, desig_type=8, length=0x1d
+03 08 00 1d
+69 71 6e 2e 32 30 32 36  2d 30 35 2e 73 67 33 0a
+58 5f 53 47 33 5f 50 52  4f 42 45 3d 31 00 00 00

commit 361c15067134cf29a66b5fa975485126f55b9aa6

sg_inq: avoid including 0-bytes in SCSI name strings
If a SCSI name string designator is null-terminated and null-padded.

According to the man page of strchr(): "The terminating null byte is considered
part of the string, so that if c is specified as '\0', these functions return a
pointer to the terminator.", I.e., sg_inq --export prints the NULL-bytes seen in
vpd_pg83 before it prints the newline character.

Fix it by properly handling the terminating null character.

Reported-by: Paul Nieleck <paul.nieleck@fujitsu.com>
Signed-off-by: Martin Wilck <mwilck@suse.com>
Fixes: 2fe1452 ("sg_inq: --export output conformance for SCSI name string and ATA fields (github: PR #83); Apply udev-conforming character encoding to VPD 0x83 designator type 8 (SCSI name string) and the T10 vendor ID ATA subfield, matching the encoding already used by designator types 0 and 1 since commit 4d770f1")

src/sg_inq.c
@@ -1723,7 +1723,7 @@ export_dev_ids(uint8_t * buff, int len, int verbose)
             }
 
             printf("SCSI_IDENT_%s_NAME=", assoc_str);
-            for (m = 0; m < i_len; ++m) {
+            for (m = 0; m < i_len && ip[m]; ++m) {
                 if ((ip[m] >= '0' && ip[m] <= '9') ||
                     (ip[m] >= 'A' && ip[m] <= 'Z') ||
                     (ip[m] >= 'a' && ip[m] <= 'z') ||